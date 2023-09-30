aa757f177aaf113d2e43244156f43cb56e0b18ff
DESKTOP-VF9OCE7\allex <allexortiz@outlook.com>
2023-09-30 14:45:36 -0500

    disabled the start button when timer is running and re-enabled it when timer runs out

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,16 +7,24 @@ function displayTime() {
     timerEl.textContent = timeRemaining;
 }
 
-startBtn.addEventListener("click", function(event) {
+startBtn.addEventListener("click", function (event) {
     event.preventDefault();
 
-    var timeInterval = setInterval(function() {
+    // Disable the start button after starting game
+    startBtn.setAttribute("disabled", "true");
+
+    var timeInterval = setInterval(function () {
         displayTime();
         timeRemaining--;
 
-        if(timeRemaining >= 0) {
+        if (timeRemaining <= 0) {
             clearInterval(timeInterval);
-            wordBlank.textContent = "GAME OVER";
+            var wordBlank = document.getElementById("wordBlank");
+            if (wordBlank) {
+                wordBlank.textContent = "GAME OVER";
         }
+        // Re-enable the start button when the game is over
+        startBtn.removeAttribute("disabled");
+    }
     }, 1000);
 });
