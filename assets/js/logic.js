bd12885cf390af7a10d962336b0a84880fa35287
DESKTOP-VF9OCE7\allex <allexortiz@outlook.com>
2023-09-30 14:21:39 -0500

    added questions to question.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,10 @@
-var timeBtn = document.getElementById("time-left");
 var startBtn = document.getElementById("start-btn");
+var timerEl = document.querySelector(".timer-count");
 
 var timeRemaining = questions.length * 15;
 
 function displayTime() {
-    timeBtn.textContent = timeRemaining;
+    timerEl.textContent = timeRemaining;
 }
 
 startBtn.addEventListener("click", function(event) {
@@ -13,5 +13,10 @@ startBtn.addEventListener("click", function(event) {
     var timeInterval = setInterval(function() {
         displayTime();
         timeRemaining--;
+
+        if(timeRemaining >= 0) {
+            clearInterval(timeInterval);
+            wordBlank.textContent = "GAME OVER";
+        }
     }, 1000);
 });
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,52 +1,54 @@
+// all of the quiz questions
+
 var questions = [
     {
-        Question: "",
-        Choices: ["A", "B", "C"],
-        Answer: ["Correct!", "Incorrect!"]
+        Question: "The condition in an 'if/else' statement is enclosed within ______.",
+        Choices: ["square brackets", "curly brackets", "parentheses"],
+        Answer: "parentheses",
     },
     {
-        Question: "",
-        Choices: ["A", "B", "C"],
-        Answer: ["Correct!", "Incorrect!"]
+        Question: "What JavaScipt method can we use to select an html element?",
+        Choices: ["document.queryselector()", "document.getElementChild", "console.log"],
+        Answer: "document.queryselector()"
     },
     {
-        Question: "",
-        Choices: ["A", "B", "C"],
-        Answer: ["Correct!", "Incorrect!"]
+        Question: "What are the two basic groups of data types in JavaScript?",
+        Choices: ["reference types and attribute", "primitive and attribute", "primitive and reference types"],
+        Answer: "primitive and reference types"
     },
     {
-        Question: "",
-        Choices: ["A", "B", "C"],
-        Answer: ["Correct!", "Incorrect!"]
+        Question: "What html tag is NOT included in the HEAD tag?",
+        Choices: ["meta", "link", "header"],
+        Answer: "header"
     },
     {
-        Question: "",
-        Choices: ["A", "B", "C"],
-        Answer: ["Correct!", "Incorrect!"]
+        Question: "Arrays in Javascript can be used to store ______.",
+        Choices: ["other arrays", "numbers & strings", "boolean", "all of the above"],
+        Answer: "all of the above"
     },
     {
-        Question: "",
-        Choices: ["A", "B", "C"],
-        Answer: ["Correct!", "Incorrect!"]
+        Question: "String values must be enclosed within ______ when being assigned to variables.",
+        Choices: ["parantheses", "quotes", "currly brackets"],
+        Answer: "quotes"
     },
     {
-        Question: "",
-        Choices: ["A", "B", "C"],
-        Answer: ["Correct!", "Incorrect!"]
+        Question: "Commonly used data types DO NOT include:",
+        Choices: ["strings", "booleans", "alerts"],
+        Answer: "alerts"
     },
     {
-        Question: "",
-        Choices: ["A", "B", "C"],
-        Answer: ["Correct!", "Incorrect!"]
+        Question: "What attribute is used in html to decorate content?",
+        Choices: ["style", "css", "src"],
+        Answer: "style"
     },
     {
-        Question: "",
-        Choices: ["A", "B", "C"],
-        Answer: ["Correct!", "Incorrect!"]
+        Question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        Choices: ["console.log", "terminal/bash", "JavaScript"],
+        Answer: "terminal/bash"
     },
     {
-        Question: "",
-        Choices: ["A", "B", "C"],
-        Answer: ["Correct!", "Incorrect!"]
-    },
-]
+        Question: "Which one is NOT a looping structure in JavaScript?",
+        Choices: ["for", "and", "do-while loops"],
+        Answer: "and"
+    }
+];
