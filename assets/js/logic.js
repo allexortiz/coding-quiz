c40d446fe85411ec0b78c9a9507974a23690e380
DESKTOP-VF9OCE7\allex <allexortiz@outlook.com>
2023-09-30 16:19:11 -0500

    got questions and answers to pop up when start button is pressed

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,10 @@
 var startBtn = document.getElementById("start-btn");
-var timerEl = document.querySelector(".timer-count");
 var questionSection = document.getElementById("question-section");
+var choicesList = document.getElementById("choices-list");
+var timerEl = document.querySelector(".timer-count");
 
+ // Keep track of the current question index
+var currentQuestionIndex = 0;
 var timeRemaining = questions.length * 15;
 
 function displayTime() {
@@ -9,27 +12,68 @@ function displayTime() {
 }
 
 function displayQuestion(question) {
-    questionSection.textContent = question;
+    questionSection.textContent = question.Question;
+    displayChoices(question.Choices);
 }
 
-startBtn.addEventListener("click", function (event) {
-    event.preventDefault();
+function displayChoices(choices) {
+    // Clear existing choices
+    choicesList.innerHTML = "";
+    // Display new choices
+    choices.forEach(function (choice, index) {
+        var choiceItem = document.createElement("li");
+        choiceItem.textContent = choice;
+        choiceItem.setAttribute("data-index", index);
+        choicesList.appendChild(choiceItem);
+    });
+}
 
-    // Disable the start button after starting game
-    startBtn.setAttribute("disabled", "true");
+function startGame() {
+    // Display the first question
+    displayQuestion(questions[currentQuestionIndex]);
 
     var timeInterval = setInterval(function () {
         displayTime();
         timeRemaining--;
 
-        if (timeRemaining <= 0) {
+        if (timeRemaining <= 0 || currentQuestionIndex >= questions.length - 1) {
             clearInterval(timeInterval);
-            var wordBlank = document.getElementById("wordBlank");
-            if (wordBlank) {
-                wordBlank.textContent = "GAME OVER";
+            // Optionally, do something when the timer reaches 0 or all questions are answered
+            // var wordBlank = document.getElementById("wordBlank");
+            // if (wordBlank) {
+            //     wordBlank.textContent = "GAME OVER";
+            // }
+            // // Re-enable the start button when the game is over
+            // startBtn.removeAttribute("disabled");
         }
-        // Re-enable the start button when the game is over
-        startBtn.removeAttribute("disabled");
-    }
     }, 1000);
+}
+
+// Event listener for choices
+choicesList.addEventListener("click", function (event) {
+    var selectedIndex = event.target.getAttribute("data-index");
+    if (selectedIndex !== null) {
+        // Handle the user's choice (compare with the correct answer, etc.)
+        console.log("User chose:", questions[currentQuestionIndex].Choices[selectedIndex]);
+        
+        // Move to the next question
+        currentQuestionIndex++;
+
+        // Display the next question (if available)
+        if (currentQuestionIndex < questions.length) {
+            displayQuestion(questions[currentQuestionIndex]);
+        } else {
+            // Optionally, do something when all questions are answered
+            console.log("All questions answered!");
+        }
+    }
+});
+
+startBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    // Disable the Start button to prevent multiple game instances
+    startBtn.setAttribute("disabled", "true");
+    
+    startGame();
 });
